d40c96e9562ca7926a315e2697332ea6c49f0252
mestery69 <48163546+mestery69@users.noreply.github.com>
2020-01-25 14:24:56 +0100

    Update example.js

--- example.js
+++ example.js
@@ -1,1 +1,18 @@
+const Paginate = require("paginate-array.js")
 
+// create some fake data
+let fakeData = new Array(42)
+	
+// create a new Paginate object
+let pager = new Paginate(fakeData)
+	
+// print all the pages
+while(pager.hasNext()) {
+  console.log(pager.next())
+}
+		
+// print first page
+console.log(pager.page(1))
+
+// print second page
+console.log(pager.next())
